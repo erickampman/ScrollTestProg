49e3dcaecdd48da2213bec4e1101e0a154fd6c12
Eric Kampman <erickampman@yahoo.com>
2017-05-04 08:40:12 -0700

    Initial Commit

---
+++ ScrollTestProg/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScrollTestProg
+//
+//  Created by Eric Kampman on 5/4/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ScrollTestProg/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScrollTestProg
+//
+//  Created by Eric Kampman on 5/4/17.
+//  Copyright © 2017 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
